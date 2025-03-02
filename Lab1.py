789c867b780fb942e13219e69b5219e3d428c8a0
stella_fff25 <stellaichme@gmail.com>
2025-03-02 14:20:31 +0200

    Lab1.3

--- Lab1.py
+++ Lab1.py
@@ -5,3 +5,11 @@ def gcd(a, b):
 
 def reverse_words(s):
     return " ".join(word[::-1] for word in s.split())
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
