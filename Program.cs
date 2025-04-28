c42bff4555af35eca5e4489a619ea7233c47bfa6
stellafff25 <stellaichme@gmail.com>
2025-04-28 23:15:30 +0300

    Lab4 test update

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const string mutexName = "MyUniqueAppMutex";
+
+        using var checker = new SingleInstanceChecker(mutexName);
+
+        if (checker.IsSingleInstance)
+        {
+            Console.WriteLine("This is the only instance running.");
+            Console.WriteLine("Press Enter to exit...");
+            Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("Another instance is already running. Exiting...");
+        }
+    }
+}
--- SingleInstanceCheckerTests.cs
+++ SingleInstanceCheckerTests.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using NUnit.Framework;
 
-namespace Lab4
+public class SingleInstanceCheckerTests
 {
-    class Tests
-    {
-    }
+	[Test]
+	public void SingleInstance_ShouldBeTrue_ForFirstInstance()
+	{
+		using var checker = new SingleInstanceChecker("TestAppMutex_First");
+		Assert.IsTrue(checker.IsSingleInstance);
+	}
+
+	[Test]
+	public void SingleInstance_ShouldBeFalse_ForSecondInstance()
+	{
+		using var checker1 = new SingleInstanceChecker("TestAppMutex_Second");
+		using var checker2 = new SingleInstanceChecker("TestAppMutex_Second");
+		Assert.IsTrue(checker1.IsSingleInstance);
+		Assert.IsFalse(checker2.IsSingleInstance);
+	}
+
+	[Test]
+	public void ShouldThrowException_WhenMutexNameIsEmpty()
+	{
+		var ex = Assert.Throws<ArgumentException>(() => new SingleInstanceChecker(""));
+		Assert.That(ex.Message, Does.StartWith("Mutex name must not be empty."));
+	}
 }
