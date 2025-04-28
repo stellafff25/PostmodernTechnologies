47de60ac551daefc9ab28fa1d1bb72548a469e73
stellafff25 <stellaichme@gmail.com>
2025-04-28 22:35:45 +0300

    Lab4

---
+++ SingleInstanceChecker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+public interface ISingleInstanceChecker : IDisposable
+{
+    bool IsSingleInstance { get; }
+}
+
+public class SingleInstanceChecker : ISingleInstanceChecker
+{
+    private readonly Mutex _mutex;
+    public bool IsSingleInstance { get; }
+
+    public SingleInstanceChecker(string mutexName)
+    {
+        if (string.IsNullOrWhiteSpace(mutexName))
+            throw new ArgumentException("Mutex name must not be empty.", nameof(mutexName));
+
+        _mutex = new Mutex(true, mutexName, out bool createdNew);
+        IsSingleInstance = createdNew;
+    }
+
+    public void Dispose()
+    {
+        if (IsSingleInstance)
+        {
+            _mutex?.ReleaseMutex();
+        }
+        _mutex?.Dispose();
+    }
+}
